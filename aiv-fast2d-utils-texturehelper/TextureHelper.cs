72a6598d2fab92ad81f7939690d9c153ddba5a0e
Luca Magagnato <biohazard911@hotmail.it>
2017-05-11 18:56:35 +0200

    Updated TextureHelper.cs

--- aiv-fast2d-utils-texturehelper/TextureHelper.cs
+++ aiv-fast2d-utils-texturehelper/TextureHelper.cs
@@ -1,16 +1,29 @@
-public static class TextureHelper
+namespace Aiv.Fast2D.Utils.TextureHelper
     {
-        public static void GenerateDecompressedTextureFromFile(string sourceFileName, string targetExtensionName = "txt")
+        public static class TextureHelper
         {
-            if (!File.Exists(sourceFileName))
-                throw new ArgumentException("File not found");
+            public static void GenerateDecompressedTextureFromFile(string sourceFileName, string targetExtensionName = "txt")
+            {
+                if (!File.Exists(sourceFileName))
+                    throw new ArgumentException("File not found");
 
-            if (string.IsNullOrEmpty(targetExtensionName))
-                throw new ArgumentException("Target Extension Name can't be null or empty");
+                if (string.IsNullOrEmpty(targetExtensionName))
+                    throw new ArgumentException("Target Extension Name can't be null or empty");
 
-            Texture texture = new Texture(sourceFileName);
-            string name = Path.GetFileNameWithoutExtension(sourceFileName);
+                Texture texture = new Texture(sourceFileName);
+                string name = Path.GetFileNameWithoutExtension(sourceFileName);
 
-            File.WriteAllBytes(name + "." + targetExtensionName, texture.Bitmap);
+                File.WriteAllBytes(name + "." + targetExtensionName, texture.Bitmap);
+            }
+
+            public static Texture LoadDecompressedTexture(int width, int height, string fileName)
+            {
+                if (!File.Exists(fileName))
+                    throw new ArgumentException("File not found");
+
+                Texture texture = new Texture(width, height);
+                texture.Update(File.ReadAllBytes(fileName));
+                return texture;
+            }
         }
     }
