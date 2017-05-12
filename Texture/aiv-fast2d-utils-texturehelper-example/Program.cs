a642d9608ca4780b023d80a0f21cd29b35400c55
Luca Magagnato <biohazard911@hotmail.it>
2017-05-12 13:38:13 +0200

    Updated TextureHelper

--- Texture/aiv-fast2d-utils-texturehelper-example/Program.cs
+++ Texture/aiv-fast2d-utils-texturehelper-example/Program.cs
@@ -3,16 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Aiv.Fast3D;
+using OpenTK;
 
 namespace Aiv.Fast2D.Utils.TextureHelper.Example
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object scale
+            float size = 4f;
 
-	class Program
-	{
-		static void Main( string[] args )
-		{
-		}
-	}
+            #region InstanceWindow
+            //Instance new window
+            Window window = new Window(1024, 768, "Test", false, 16, 4);
+            #endregion
+
+            #region DecompressTexture
+            /* 
+             * Decompress Texture from single file
+             * This method has to be called only once,
+             * because it could slows down the game,
+             * do decompression in single project which is not
+             * a part of your own working project.
+             */
+            TextureHelper.GenerateDecompressedTextureFromFile("Textures/GasFuel.png");
+
+            //Decompress Textures from list of files (Has to be called only once)
+            //TextureHelper.GenerateDecompressedTexturesFromFolder("Textures", "Decompressed");
+            #endregion
+
+            #region LoadDecompressedTexture
+            //Return decompressed texture and save it.
+            Texture loadedTexture = TextureHelper.LoadDecompressedTexture(2048, 2048, "GasFuel.txt");
+            #endregion
+
+            #region LoadOBJModel
+            //Create a Mesh3[] to load custo ".obj"'s model
+            Mesh3[] meshes = ObjLoader.Load("Models/GasFuel.obj", Vector3.One * size);
+            #endregion
+
+            #region GameLoop
+            while (window.opened)
+            {
+                //Loop through array and draw texture.
+                foreach (var item in meshes)
+                {
+                    item.DrawTexture(loadedTexture);
+                }
+
+                //Break while loop
+                if (window.GetKey(KeyCode.Esc))
+                    break;
+
+                window.Update();
+            }
+            #endregion
+        }
+    }
 }
--- Texture/aiv-fast2d-utils-texturehelper/TextureHelper.cs
+++ Texture/aiv-fast2d-utils-texturehelper/TextureHelper.cs
@@ -3,73 +3,73 @@ using System.IO;
 
 namespace Aiv.Fast2D.Utils.TextureHelper
 {
-	public static class TextureHelper
-	{
-		/// <summary>
-		/// It will generates a single decompressed texture inside giving it's source name path.
-		/// </summary>
-		/// <param name="sourceFileName">Name of the Texture to Decompress</param>
-		/// <param name="targetExtensionName">Name of extension the file will be created with</param>
-		public static void GenerateDecompressedTextureFromFile( string sourceFileName, string targetExtensionName = "txt" )
-		{
-			if ( !File.Exists( sourceFileName ) )
-				throw new ArgumentException( "File not found" );
+    public static class TextureHelper
+    {
+        /// <summary>
+        /// It will generates a single decompressed texture inside giving it's source name path.
+        /// </summary>
+        /// <param name="sourceFileName">Name of the Texture to Decompress</param>
+        /// <param name="targetExtensionName">Name of extension the file will be created with</param>
+        public static void GenerateDecompressedTextureFromFile(string sourceFileName, string targetExtensionName = "txt")
+        {
+            if (!File.Exists(sourceFileName))
+                throw new ArgumentException("File not found");
 
-			if ( string.IsNullOrEmpty( targetExtensionName ) )
-				throw new ArgumentException( "Target Extension Name can't be null or empty" );
+            if (string.IsNullOrEmpty(targetExtensionName))
+                throw new ArgumentException("Target Extension Name can't be null or empty");
 
-			Texture texture = new Texture( sourceFileName );
-			string name = Path.GetFileNameWithoutExtension( sourceFileName );
+            Texture texture = new Texture(sourceFileName);
+            string name = Path.GetFileNameWithoutExtension(sourceFileName);
 
-			File.WriteAllBytes( name + "." + targetExtensionName, texture.Bitmap );
-		}
+            File.WriteAllBytes(name + "." + targetExtensionName, texture.Bitmap);
+        }
 
-		/// <summary>
-		/// It will generates a list of decompressed textures inside a new directory giving it's folder path.
-		/// </summary>
-		/// <param name="targetDirectory">Directory which contains list of textures to decompress</param>
-		/// <param name="newDirectoryName">Directory Which has to be created in order to place all the new decompressed textures</param>
-		/// <param name="targetExtensionName">Extension which the file will be created</param>
-		/// <param name="recursiveCheck">A recursive loop checking through all sub directories</param>
-		public static void GenerateDecompressedTexturesFromFolder( string targetDirectory, string newDirectoryName, string targetExtensionName = "txt", bool recursiveCheck = false )
-		{
-			if ( string.IsNullOrEmpty( targetDirectory ) || string.IsNullOrEmpty( newDirectoryName ) )
-				throw new Exception( "targetDir or newDirName can't be null or empty" );
+        /// <summary>
+        /// It will generates a list of decompressed textures inside a new directory giving it's folder path.
+        /// </summary>
+        /// <param name="targetDirectory">Directory which contains list of textures to decompress</param>
+        /// <param name="newDirectoryName">Directory Which has to be created in order to place all the new decompressed textures</param>
+        /// <param name="targetExtensionName">Extension which the file will be created</param>
+        /// <param name="recursiveCheck">A recursive loop checking through all sub directories</param>
+        public static void GenerateDecompressedTexturesFromFolder(string targetDirectory, string newDirectoryName, string targetExtensionName = "txt", bool recursiveCheck = false)
+        {
+            if (string.IsNullOrEmpty(targetDirectory) || string.IsNullOrEmpty(newDirectoryName))
+                throw new Exception("targetDir or newDirName can't be null or empty");
 
-			string[] entriesFiles = Directory.GetFiles( targetDirectory );
-			Directory.CreateDirectory( newDirectoryName );
-			foreach ( string fileName in entriesFiles )
-			{
-				Texture texture = new Texture( fileName );
-				string singleFileName = Path.GetFileNameWithoutExtension( fileName );
-				File.WriteAllBytes( newDirectoryName + "/" + singleFileName + "." + targetExtensionName, texture.Bitmap );
-			}
+            string[] entriesFiles = Directory.GetFiles(targetDirectory);
+            Directory.CreateDirectory(newDirectoryName);
+            foreach (string fileName in entriesFiles)
+            {
+                Texture texture = new Texture(fileName);
+                string singleFileName = Path.GetFileNameWithoutExtension(fileName);
+                File.WriteAllBytes(newDirectoryName + "/" + singleFileName + "." + targetExtensionName, texture.Bitmap);
+            }
 
-			if ( !recursiveCheck )
-				return;
+            if (!recursiveCheck)
+                return;
 
-			string[] subDir = Directory.GetDirectories( targetDirectory );
-			foreach ( string subdirectory in subDir )
-			{
-				TextureHelper.GenerateDecompressedTexturesFromFolder( subdirectory, newDirectoryName, targetExtensionName, true );
-			}
-		}
+            string[] subDir = Directory.GetDirectories(targetDirectory);
+            foreach (string subDirectory in subDir)
+            {
+                GenerateDecompressedTexturesFromFolder(subDirectory, newDirectoryName, targetExtensionName, true);
+            }
+        }
 
-		/// <summary>
-		/// It will loads a decompressed texture.
-		/// </summary>
-		/// <param name="width">Texture width</param>
-		/// <param name="height">Texture height</param>
-		/// <param name="fileName">Name of decompressed file</param>
-		/// <returns></returns>
-		public static Texture LoadDecompressedTexture( int width, int height, string fileName )
-		{
-			if ( !File.Exists( fileName ) )
-				throw new ArgumentException( "File not found" );
+        /// <summary>
+        /// It will loads a decompressed texture.
+        /// </summary>
+        /// <param name="width">Texture width</param>
+        /// <param name="height">Texture height</param>
+        /// <param name="fileName">Name of decompressed file</param>
+        /// <returns></returns>
+        public static Texture LoadDecompressedTexture(int width, int height, string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new ArgumentException("File not found");
 
-			Texture texture = new Texture( width, height );
-			texture.Update( File.ReadAllBytes( fileName ) );
-			return texture;
-		}
-	}
+            Texture texture = new Texture(width, height);
+            texture.Update(File.ReadAllBytes(fileName));
+            return texture;
+        }
+    }
 }
